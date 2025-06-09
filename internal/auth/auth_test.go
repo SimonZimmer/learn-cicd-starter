3d202f38c79a5855d9b983e2b4699da8fb3ea6fd
Simon Zimmermann <mail@simonzimmermann.de>
2025-06-09 09:27:37 +0200

    apply formatting

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,33 +2,33 @@ package auth
 
 import (
 	"net/http"
-    "testing"
+	"testing"
 )
 
 func TestGetAPIKey_empty_header(t *testing.T) {
-    header := http.Header{}
-    got1, got2 := GetAPIKey(header)
-    want1, want2 := "", ErrNoAuthHeaderIncluded
-    if want1 != got1 {
-        t.Fatalf("expected: %v, got: %v", want1, got1)
-    }
+	header := http.Header{}
+	got1, got2 := GetAPIKey(header)
+	want1, want2 := "", ErrNoAuthHeaderIncluded
+	if want1 != got1 {
+		t.Fatalf("expected: %v, got: %v", want1, got1)
+	}
 
-    if want2 != got2 {
-        t.Fatalf("expected: %v, got: %v", want2, got2)
-    }
+	if want2 != got2 {
+		t.Fatalf("expected: %v, got: %v", want2, got2)
+	}
 }
 
 func TestGetAPIKey_malformed(t *testing.T) {
-    header := http.Header{
-        "Authorization": []string{"ApiKeymy-secret-key"},
+	header := http.Header{
+		"Authorization": []string{"ApiKeymy-secret-key"},
 	}
-    got1, got2 := GetAPIKey(header)
-    want1, want2msg := "", "malformed authorization header"
-    if want1 != got1 {
-        t.Fatalf("expected: %v, got: %v", want1, got1)
-    }
+	got1, got2 := GetAPIKey(header)
+	want1, want2msg := "", "malformed authorization header"
+	if want1 != got1 {
+		t.Fatalf("expected: %v, got: %v", want1, got1)
+	}
 
-    if want2msg != got2.Error() {
-        t.Fatalf("expected: %v, got: %v", want2msg, got2)
-    }
+	if want2msg != got2.Error() {
+		t.Fatalf("expected: %v, got: %v", want2msg, got2)
+	}
 }
